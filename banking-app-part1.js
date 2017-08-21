aaa9a2772f32e1c6eadb0070a8ea9e18931b88b7
recursive-web <31081796+recursive-web@users.noreply.github.com>
2017-08-21 13:26:18 +1000

    Create banking-app-part1.js

---
+++ banking-app-part1.js
@@ -0,0 +1,19 @@
+var account = {
+  balance: 0
+};
+
+function deposit(account, amount) {
+  account.balance += amount;  
+}
+
+function withdraw(account, amount) {
+  if (amount > account.balance) {
+    console.log('insufficient funds');
+    return;
+  }
+  account.balance -= amount;
+}
+
+function getBalance(account){
+  return account.balance;
+}
